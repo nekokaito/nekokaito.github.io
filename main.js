0bccc084f597569c0928a638374bb5e29ca1db8a
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2023-10-16 07:07:55 +0600

    Update Index

---
+++ main.js
@@ -0,0 +1,20 @@
+var myAudio = document.getElementById("myAudio");
+var musicmute = document.getElementById("icon");
+
+function muteUnmute() {
+  if (myAudio.muted === true) {
+    myAudio.muted = false;
+    musicmute.classList.add("fa-music");
+    musicmute.classList.remove("fa-volume-xmark");
+    
+    
+    myAudio.play();
+  } else {
+    myAudio.muted = true;
+    
+
+    musicmute.classList.add("fa-volume-xmark");
+    musicmute.classList.remove("fa-music");
+  }
+
+}
