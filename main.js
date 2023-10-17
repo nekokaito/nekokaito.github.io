de1942a5b8aa12705c062d9803197007296bd18b
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2023-10-17 11:46:45 +0600

    added

--- main.js
+++ main.js
@@ -6,13 +6,9 @@ function muteUnmute() {
     myAudio.muted = false;
     musicmute.classList.add("fa-music");
     musicmute.classList.remove("fa-volume-xmark");
-    
-    
     myAudio.play();
   } else {
     myAudio.muted = true;
-    
-
     musicmute.classList.add("fa-volume-xmark");
     musicmute.classList.remove("fa-music");
   }
